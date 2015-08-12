89671f6cd24637ba45aaaba26a951740cc01a3a6
Ashim Shahi <ashim.shahi@gmail.com>
2015-08-12 17:41:26 +0200

    Fix js for initial attempt.

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
 	$("img").click(function() {
-		alert($(this).attr("id"));
         $.ajax({
-        	url : 'http://localhost/gravaHal/updateGame',
             data : {
             	changedIndex: $(this).attr("id"),
-            	lastState: null
             },
             success : function(responseText) {
                 location.reload();
